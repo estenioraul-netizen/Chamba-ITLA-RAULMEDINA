1f72af8c417622a921e46c15c0ab9ed2b6721afb
estenioraul-netizen <estenioraul@gmail.com>
2026-02-08 14:11:09 -0400

    practica de par o impar
    practica de par o impar

---
+++ PAR O IMPAR.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class chamba
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Hola, escribe un numero y te dire si es par o impar");
+        int numero = int.Parse(Console.ReadLine());
+
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El numero es par");
+        }
+        else
+        {
+            Console.WriteLine("El numero es impar");
+        }
+
+    }
+}
